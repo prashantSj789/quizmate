2a16ddc20e38bad6ef07db1901d183c3bfac18b1
prashantSj789 <singhrashant79072@gmail.com>
2024-03-01 22:26:49 +0530

    "dummy data file"

--- lib/Screens/QuizScreen.dart
+++ lib/Screens/QuizScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuizScreen extends StatefulWidget {
   const QuizScreen({super.key});
@@ -8,13 +9,29 @@ class QuizScreen extends StatefulWidget {
 }
 
 class _QuizScreenState extends State<QuizScreen> {
+  
   @override
   Widget build(BuildContext context) {
+    final height= MediaQuery.sizeOf(context).height;
+    final width = MediaQuery.sizeOf(context).width;
     return Scaffold(
       appBar: AppBar(
       backgroundColor: Colors.blueAccent,
-      title: Text("All the Best"),
+      title: Center(child: Text("All the Best",style: GoogleFonts.rye(color: Colors.black,fontWeight: FontWeight.bold),)),
       ),
+      body: ListView(
+        children: [
+          SizedBox(
+            height: height,
+            width: width,
+            child: Card(
+                  elevation: 5,
+                    
+            ),
+          )
+        ],
+      )
+    ,
     );
   }
 }
